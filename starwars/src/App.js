21ed72d27691e2c0d2db1e1f617e9711bcea709b
JabinWashburn <jabinwashburn@gmail.com>
2020-03-20 11:36:36 -0700

    finished project

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import './App.css'
 import Content from './components/Content'
-import Characters from './components/Content'
 
 
 export default function App () {
--- starwars/src/components/Content.js
+++ starwars/src/components/Content.js
@@ -3,18 +3,21 @@ import styled from 'styled-components'
 
 const Container = styled.div`
     border: 2px solid black;
+    max-width: 20%;
+    display: flex;
+    flex-direction: column;
+    margin: 3% 0% 0% 40%;
+    background-color: #bbc3c5;
+    border-radius: 15px;
 `
 
 
 const Content = props => {
     return (
         <Container>
-            
-            <p>{props.name}</p>
-            <p>{props.birth_year}</p>
-            <p>{props.homeworld}</p>
-            <p>{props.films}</p>
-            <p>{props.starships}</p>
+            <p>Name = {props.name}</p>
+            <p>Birth = {props.birth}</p> 
+            <p>Mass = {props.mass}</p>
         </Container>
 
     )
